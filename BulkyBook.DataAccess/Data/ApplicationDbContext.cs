0521e75b02cee6d7859897dcdeb9169dc9950e98
Hugo Simões <hugo.mn.simoes@gmail.com>
2023-03-07 17:16:18 +0000

    Extended IdentityUser to include more fields. Add Company model.

--- BulkyBook.DataAccess/Data/ApplicationDbContext.cs
+++ BulkyBook.DataAccess/Data/ApplicationDbContext.cs
@@ -15,6 +15,8 @@ public class ApplicationDbContext : IdentityDbContext<IdentityUser>
     public DbSet<Category> Categories { get; set; }
     public DbSet<CoverType> CoverTypes { get; set; }
     public DbSet<Product> Products { get; set; }
+    public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+
 
     protected override void OnModelCreating(ModelBuilder builder)
     {
