b3c69e72dedbcada185ee17ae28b05b02a4c748d
Hugo Simões <hugo.mn.simoes@gmail.com>
2023-03-20 17:31:34 +0000

    Added OrderController button functionality and PaymentConfirmation.

--- BulkyBook.DataAccess/Repository/Repository.cs
+++ BulkyBook.DataAccess/Repository/Repository.cs
@@ -19,6 +19,7 @@ namespace BulkyBook.DataAccess.Repository
         public Repository(ApplicationDbContext db)
         {
             _db = db;
+            //_db.ShoppingCarts.AsNoTracking().FirstOrDefault
             this.dbSet = _db.Set<T>();
         }
 
@@ -42,9 +43,18 @@ namespace BulkyBook.DataAccess.Repository
             return query.ToList();
         }
 
-        public T GetFirstOrDefault(Expression<Func<T, bool>> filter, string? includeProperties = null)
+        public T GetFirstOrDefault(Expression<Func<T, bool>> filter, string? includeProperties = null, bool tracked = true)
         {
-            IQueryable<T> query = dbSet;
+            IQueryable<T> query;
+
+            if (tracked)
+            {
+                query = dbSet;
+            }
+            else
+            {
+                query = dbSet.AsNoTracking();
+            }
 
             query = query.Where(filter);
 
--- BulkyyBookWeb/Areas/Admin/Controllers/OrderController.cs
+++ BulkyyBookWeb/Areas/Admin/Controllers/OrderController.cs
@@ -38,6 +38,130 @@ public class OrderController : Controller
         return View(orderVM);
     }
 
+    [ActionName("Details")]
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Details_PayNow()
+    {
+        orderVM.OrderHeader = _unitOfWork.OrderHeader.GetFirstOrDefault(l => l.Id == orderVM.OrderHeader.Id, tracked: false);
+        orderVM.OrderDetails = _unitOfWork.OrderDetail.GetAll(l => l.OrderId == orderVM.OrderHeader.Id, includeProperties: "Product");
+
+        // Add Stripe payment code
+
+        return RedirectToAction("PaymentConfirmation", new { orderHeaderId = orderVM.OrderHeader.Id });
+    }
+
+    public IActionResult PaymentConfirmation(int orderHeaderId)
+    {
+        OrderHeader orderHeader = _unitOfWork.OrderHeader.GetFirstOrDefault(l => l.Id == orderHeaderId);
+
+        if (orderHeader.PaymentStatus == SD.PaymentStatusDelayedPayment)
+        {
+            //Add Stripe Get Session Code
+            bool paymentPaid = true;
+
+            if (paymentPaid)
+            {
+                _unitOfWork.OrderHeader.UpdateStatus(orderHeaderId, orderHeader.OrderStatus, SD.PaymentStatusApproved);
+                _unitOfWork.Save();
+            }
+        }
+
+        return View(orderHeaderId);
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult UpdateOrderDetail()
+    {
+        var orderHeaderFromDB = _unitOfWork.OrderHeader.GetFirstOrDefault(l => l.Id == orderVM.OrderHeader.Id, tracked: false);
+
+        orderHeaderFromDB.Name = orderVM.OrderHeader.Name;
+        orderHeaderFromDB.PhoneNumber = orderVM.OrderHeader.PhoneNumber;
+        orderHeaderFromDB.StreetAddress = orderVM.OrderHeader.StreetAddress;
+        orderHeaderFromDB.City = orderVM.OrderHeader.City;
+        orderHeaderFromDB.State = orderVM.OrderHeader.State;
+        orderHeaderFromDB.PostalCode = orderVM.OrderHeader.PostalCode;
+
+        if (orderVM.OrderHeader.Carrier != null)
+        {
+            orderHeaderFromDB.Carrier = orderVM.OrderHeader.Carrier;
+        }
+
+        if (orderVM.OrderHeader.TrackingNumber != null)
+        {
+            orderHeaderFromDB.TrackingNumber = orderVM.OrderHeader.TrackingNumber;
+        }
+
+        _unitOfWork.OrderHeader.Update(orderHeaderFromDB);
+        _unitOfWork.Save();
+
+        TempData["success"] = "Order Details were updated successfully";
+
+        return RedirectToAction("Details", "Order", new { orderId = orderHeaderFromDB.Id });
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult StartProcessing()
+    {
+        _unitOfWork.OrderHeader.UpdateStatus(orderVM.OrderHeader.Id, SD.StatusInProcess);
+        _unitOfWork.Save();
+
+        TempData["success"] = "Order status was updated successfully";
+
+        return RedirectToAction("Details", "Order", new { orderId = orderVM.OrderHeader.Id });
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult ShipOrder()
+    {
+        var orderHeader = _unitOfWork.OrderHeader.GetFirstOrDefault(l => l.Id == orderVM.OrderHeader.Id, tracked: false);
+
+        orderHeader.TrackingNumber = orderVM.OrderHeader.TrackingNumber;
+        orderHeader.Carrier = orderVM.OrderHeader.Carrier;
+        orderHeader.OrderStatus = SD.StatusShipped;
+        orderHeader.ShippingDate = DateTime.Now;
+
+        if (orderHeader.PaymentStatus == SD.PaymentStatusDelayedPayment)
+        {
+            orderHeader.PaymentDueDate = DateTime.Now.AddDays(30);
+        }
+
+        _unitOfWork.OrderHeader.Update(orderHeader);
+        _unitOfWork.Save();
+
+        TempData["success"] = "Order shipped successfully";
+
+        return RedirectToAction("Details", "Order", new { orderId = orderVM.OrderHeader.Id });
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    [Authorize(Roles = SD.Role_Admin + "," + SD.Role_Employee)]
+    public IActionResult CancelOrder()
+    {
+        var orderHeader = _unitOfWork.OrderHeader.GetFirstOrDefault(l => l.Id == orderVM.OrderHeader.Id, tracked: false);
+
+        if (orderHeader.PaymentStatus == SD.PaymentStatusApproved)
+        {
+            // Stripe code for refund
+
+            _unitOfWork.OrderHeader.UpdateStatus(orderHeader.Id, SD.StatusCancelled, SD.StatusRefunded);
+        }
+        else
+        {
+            _unitOfWork.OrderHeader.UpdateStatus(orderHeader.Id, SD.StatusCancelled, SD.StatusCancelled);
+        }
+
+        _unitOfWork.Save();
+
+        TempData["success"] = "Order cancelled successfully";
+
+        return RedirectToAction("Details", "Order", new { orderId = orderVM.OrderHeader.Id });
+    }
+
     #region API Calls
 
     [HttpGet]
